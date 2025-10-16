bb9e63e02f0b7f42421fc53064f6279540c7e381
mphamtran <mphamtran100@gmail.com>
2025-10-16 15:17:09 -0700

    Loadout Manager Python Terminal Script
    -HD2EquipmentManager.py: utilizes pyautogui to scan screen and store coordinates for equipment, pydirectinput handles inputs. Waits until you've entered the equipment screen (dropping into the hellpod) and then loops through each equipment slots and equips each equipment
    -boxPosFinder.py: script to find position and dimensions for pyautogui box
    -mousePosFinder: script to display mouse position
    -Added all offensive equipment and the plas45epoch supply equipment images to icons folder
    Notes:
    -Currently slots are hardcoded to 1.png, 2.png, 3.png, 4.png, 5.png. The plans are to decouple these and then upgrade from the terminal to a tkinter gui
    -Need to add supply and defensive icons along side offensive icons to icons folder

---
+++ Python and Dependency Setup.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+python -V
+PAUSE
+pip install PyScreeze
+
+pip install pywin32
+
+pip install keyboard
+
+pip install pyautogui
+
+pip install opencv-python
+
+pip install Pillow --upgrade
+
+ECHO Installed/Updated: Python, PyScreeze, pywin32, keyboard, pyautogui, opencv-python, Pillow
+PAUSE
